06b17cea04875f0d6bf8dad4478f77b95c77b0f9
Murilo Silva <murilo.alex.silva@gmail.com>
2021-08-17 22:24:22 -0300

    termino

--- aula2/ex4.js
+++ aula2/ex4.js
@@ -1,10 +1,15 @@
 let nome = ['José da Silva', 'Carlos de Souza', 'Aline Ferreira', 'Ana Paula'];
-let idade = [27,28,33,26];
-let peso = [83.5,80.1,63.7,55.0];
-let altura = [1.70,1.76,1.53,1.62];
-let plano = ['Ouro','Diamante',null,'Prata'];
-let imc = peso / (altura*altura);
-console.log(nome[0] + ' tem ' + idade[0] + ' anos e seu índice de massa corporal é de ' + ' com o plano ' + plano[0]);
-console.log(nome[1] + ' tem ' + idade[1] + ' anos e seu índice de massa corporal é de ' + ' com o plano ' + plano[1]);
-console.log(nome[2] + ' tem ' + idade[2] + ' anos e seu índice de massa corporal é de ' + ' com o plano ' + plano[2]);
-console.log(nome[3] + ' tem ' + idade[3] + ' anos e seu índice de massa corporal é de ' + ' com o plano ' + plano[3]);
+let idade = [27, 28, 33, 26];
+let peso = [83.5, 80.1, 63.7, 55.0];
+let altura = [1.70, 1.76, 1.53, 1.62];
+let plano = ['Ouro', 'Diamante', null, 'Prata'];
+
+console.log(nome[0] + ' tem ' + idade[0] + ' anos e seu índice de massa corporal é de ' + imc(peso[0], altura[0]).toFixed(2) + ' com o plano ' + plano[0]);
+console.log(nome[1] + ' tem ' + idade[1] + ' anos e seu índice de massa corporal é de ' + imc(peso[1], altura[1]).toFixed(2) + ' com o plano ' + plano[1]);
+console.log(nome[2] + ' tem ' + idade[2] + ' anos e seu índice de massa corporal é de ' + imc(peso[2], altura[2]).toFixed(2) + ' com o plano ' + plano[2]);
+console.log(nome[3] + ' tem ' + idade[3] + ' anos e seu índice de massa corporal é de ' + imc(peso[3], altura[3]).toFixed(2) + ' com o plano ' + plano[3]);
+
+function imc(peso, altura) {
+    let imc = peso / (altura * altura);
+    return imc;
+}
--- aula5/ex1.js
+++ aula5/ex1.js
@@ -1,41 +1,59 @@
 
-//Polegada para centimetro
+//Polegada para centimetro - 1 
 function polegadaParaCentimetro(numA) {
     var resultado = numA * 2.54;
     return console.log(numA + ' polegada será ' + resultado + ' em centimetro.');
 }
 polegadaParaCentimetro(2);
 
-//String para URL
+//String para URL - 2
 function stringParaUrl(stringA) {
     return console.log('http://www.' + stringA + '.com.br');
 }
 stringParaUrl('digitalhouse');
 
-//  String com Caractere
+//  String com Caractere -3
 function stringComCaractere(stringA) {
     return console.log(stringA + '!');
 }
 stringComCaractere("Alerta");
 
-//idade dos Cachorros
+//idade dos Cachorros -4
 function idadeCachorro(idade) {
     let idadeCachorro = idade * 7;
     return console.log('Seu dog possui ' + idadeCachorro + ' anos em idade humana!!!');
 }
 idadeCachorro(5);
 
-//Calculo hora de trabalho
+//Calculo hora de trabalho -5
 function horaTrabalho(salario) {
     let valorHora = salario / 160;
     return console.log('Seu salário é R$' + salario + ',00 e o valor da sua hora é R$' + valorHora + ',00');
 }
 horaTrabalho(1600);
 
-//Calculo imc
+//Calculo imc -6
 function calculoImc(peso, altura) {
     altura = altura / 100;
     let imc = peso / (altura * altura);
     return console.log('Seu IMC é ' + imc.toFixed(2));
 }
 calculoImc(80, 170);
+
+//String para Maiuscula -7
+function stringMaiuscula(string) {
+    return console.log(string.toUpperCase());
+}
+stringMaiuscula('murilo');
+
+//Tipo de dado - 8
+function tipoDeDado(dado) {
+    return console.log(typeof dado);
+}
+tipoDeDado('texto');
+
+//Digite o raio de um circulo - 9
+function circunferencia(raio) {
+    return console.log(2 * Math.PI * raio);
+}
+circunferencia(10);
