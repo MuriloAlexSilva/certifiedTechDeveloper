ca9d87b168a28ce4c8fc76385177575d85d1767c
Murilo Silva <murilo.alex.silva@gmail.com>
2021-08-23 06:39:21 -0300

    aula8

---
+++ programacaoImperativa/aula7/condicoes-if.js
@@ -0,0 +1,9 @@
+let dia = 'sabado';
+let clima = 'chuva';
+if(dia == 'domingo' && clima == 'sol'){
+    console.log('Vou para a praia!');
+}else if(dia == 'domingo' && clima == 'chuva'){
+    console.log('Vou no cinema!');
+}else{
+    console.log('Fico em casa!');
+}
