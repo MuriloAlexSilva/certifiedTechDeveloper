922b33824ddc5ea33deebcea5ff1116d009efd52
Murilo Silva <murilo.alex.silva@gmail.com>
2021-08-30 20:43:04 -0300

    .

---
+++ programacaoImperativa/aula10/mesaTrabalho.js
@@ -0,0 +1,56 @@
+// let textos = ['texto1','texto2','texto3','texto4','texto5'];
+// console.log(textos);
+// textos[0] = 'text';
+// console.log(textos);
+// textos.push('texto6');
+// console.log(textos);
+// textos.unshift('texto0');
+// console.log(textos);
+// textos.pop();
+// console.log(textos);
+// textos.shift();
+// console.log(textos);
+
+// let nomes = ['texto1', 'textoo']
+
+// if (textos[0] == nomes[1]){
+//     console.log('Similar');
+// }else{
+//     console.log('Diferente');
+// }
+
+// let numbers =[22, 33, 54, 66, 72]
+// console.log(numbers[numbers.length-1])
+
+// let grupoDeAmigos = [ 
+//     ["Harry", "Ron", "Hermione"], 
+//     ["Spiderman", "Hulk", "Ironman"],   
+//     ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ]
+// console.log(grupoDeAmigos[0][0])
+
+// let str = 'una string qualquer';
+// let grupoDeAmigos = [ [45, 89, 0], ["Digital", "House", true], ["string", "123","false", 54, true, str] ]
+// console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1])
+
+// function imprimirInverso(num) {
+//     console.log(num[num.length-1]);
+//     console.log(num[num.length-2]);
+//     console.log(num[num.length-3]);
+//     console.log(num[num.length-4]);
+    
+// }
+// let numeros = [1,2,3,7,6];
+// imprimirInverso	(numeros)
+
+// function imprimirInverso(num) {
+//     for (let i = num.length-1; i >=0; i--) {
+//       console.log(num[i]);
+        
+//     }
+// }
+// let numeros = [1,2,3,7,6];
+// imprimirInverso	(numeros);
+
+function inverter(num) {
+  
+}
