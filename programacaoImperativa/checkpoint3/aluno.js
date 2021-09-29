97cbf8ceb818e1ad523dd47f195fd3925f630665
Murilo Silva <murilo.alex.silva@gmail.com>
2021-09-29 16:48:09 -0300

    .

--- programacaoImperativa/checkpoint3/aluno.js
+++ programacaoImperativa/checkpoint3/aluno.js
@@ -4,8 +4,25 @@ function Aluno (_nome, _qtddFaltas, _notas){
     this.nome = _nome;
     this.qtddFaltas = _qtddFaltas;
     this.notas = _notas;
-    `Seu nome é ${this.nome} e possui ${this.qtddFaltas} faltas e suas notas são ${this.notas}`
+    console.log(`Seu nome é ${this.nome} e possui ${this.qtddFaltas} faltas e suas notas são ${this.notas}`);
     //funções aqui
+    function calcularMedia(this.notas) {
+        let contadorNotas = 0;
+        let somaDasNotas = 0;
+    
+        for (let i = 0; i < notas.length; i++) {
+            if (notas[i]) {
+                contadorNotas++;
+                somaDasNotas += notas[i];
+          
+            }
+            
+        }
+    
+        let media = somaDasNotas/contadorNotas;
+        return media;
+    }
+    console.log(calcularMedia())
 
 }
 
--- programacaoImperativa/checkpoint3/index.js
+++ programacaoImperativa/checkpoint3/index.js
@@ -1,7 +1,8 @@
 let teste = require('./aluno');
 
-let sd = new teste.Aluno('Nome',5,[7,57])
-console.log(sd.nome)
+// let sd = new teste.Aluno('Nome',5,[7,57])
+// console.log(sd.nome)
 
+teste.Aluno('Murilo',5,[5,4,6,7])
 
 
