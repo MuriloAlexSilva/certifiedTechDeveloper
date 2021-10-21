baa772b67002ca1bb0e655a7c2dd36eca5f98520
Murilo Silva <murilo.alex.silva@gmail.com>
2021-10-21 12:09:14 -0300

    js

--- livro/ex2.js
+++ livro/ex2.js
@@ -33,7 +33,18 @@ console.log(teste3);
 teste3.sort();
 console.log(teste3);
 
+
+// O indexOf retorna o primeiro valor encontrado no Array
+// já o lastIndexOf retorna o ultimo valor encontrado no Array
+// ---Isto seria para os casos de valores repetidos dentro do array
+
+
 let teste4 = [0,1,2,3,4,5,6];
-console.log(teste4.indexOf(0))
-let string = teste4.indexOf(2) == -1 ? 'Não existe este numero': teste4.indexOf(2);
+console.log(teste4.indexOf(6))// Consulta o item dentro do () no array mencionado
+let string = teste4.indexOf(7) == -1 ? 'Não existe este numero no array': teste4.indexOf(7);
+//Qdo não encontra o valor, o indexOf retorna -1, neste caso estou mencionando
+// que se der -1 irá aparecer a mensagem de Não existe este numero no array
 console.log(string)
+
+
+//---------------------------------------------------------------------------------------------
